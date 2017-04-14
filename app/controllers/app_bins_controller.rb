319d0f00c0dc17e5a57b0ddfe98f317638562a36
Tyler Guillen <tyguillen@gmail.com>
2017-04-14 14:50:13 -0400

    basic controller action stats display

--- app/controllers/app_bins_controller.rb
+++ app/controllers/app_bins_controller.rb
@@ -2,6 +2,34 @@ class AppBinsController < ApplicationController
   before_action :set_app_bin
 
   def show
+    sql = <<~SQL
+      SELECT c, a, event_type, AVG(tot_duration) AS avg_time_spent, ROUND(AVG(event_count)) AS avg_count, COUNT(*) AS total_requests
+        FROM (
+          SELECT t_id, ca.controller AS c, ca.action AS a, e.event_type, AVG(e.duration) AS avg_duration, SUM(e.duration) AS tot_duration, COUNT(e.duration) AS event_count
+            FROM transaction_events AS e
+            INNER JOIN (
+              SELECT cycle_transaction_id AS t_id, data->>'controller' AS controller, data->>'action' AS action
+                FROM transaction_events
+                WHERE event_type = 'controller_action'
+            ) AS ca
+              ON ca.t_id = e.cycle_transaction_id
+            GROUP BY t_id, ca.controller, ca.action, e.event_type
+        ) AS totals
+        GROUP BY c, a, event_type
+        ORDER BY total_requests DESC, c, a, event_type;
+    SQL
+
+    tuples = ActiveRecord::Base.connection.execute sql
+    @actions = {}
+    tuples.each do |tuple|
+      c_a = "#{tuple['c']}##{tuple['a']}"
+      @actions[c_a] ||= {}
+      event_values = Hash[tuple.slice('avg_time_spent', 'avg_count', 'total_requests').
+        map do |k, v|
+          [k, v.to_i == v.to_f ? v.to_i : v.to_f]
+        end]
+      @actions[c_a][tuple['event_type']] = event_values
+    end
   end
 
   def new
