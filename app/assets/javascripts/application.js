93c0c889db5ea42ba607c151e81b17f5c1580d3a
Tyler Guillen <tyguillen@gmail.com>
2017-04-24 11:50:19 -0400

    background jobs index data now in its own service object

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,7 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
+// At this time we are not using a front-end framework aside from JQuery. Note
+// that the `require_tree` directive is absent, so if you add any JS files that
+// you wish to be loaded into the compiled JS, please add them individually
+// here, in the order in which you would like it to be loaded.
 //
 //= require jquery
 //= require jquery_ujs
---
+++ app/services/background_jobs_index_data.rb
@@ -0,0 +1,62 @@
+class BackgroundJobsIndexData
+  def initialize(app_bin_id)
+    @app_bin_id = app_bin_id
+  end
+
+  def fetch!
+    tuples = fetch_tuples
+    process_tuples tuples
+  end
+
+  private
+
+  def fetch_tuples
+    sql = <<~SQL
+      SELECT
+        name AS job_name,
+        avg(duration) AS avg_duration,
+        count(*) AS hits,
+        avg((
+          SELECT sum(duration)
+          FROM jsonb_to_recordset(events->'sql') AS x(duration NUMERIC)
+        )) AS avg_time_in_sql,
+        avg((
+          SELECT sum(duration)
+          FROM jsonb_to_recordset(events->'view') AS x(duration NUMERIC)
+        )) AS avg_time_in_view,
+        avg((
+          SELECT sum(duration)
+          FROM
+            jsonb_to_recordset(events->'controller_action')
+              AS x(duration NUMERIC)
+        )) AS avg_time_in_controller,
+        avg((
+          SELECT sum(duration)
+          FROM jsonb_to_recordset(events->'other') AS x(duration NUMERIC)
+        )) AS avg_time_in_other
+      FROM cycle_transactions
+      WHERE
+        app_bin_id = #{ActiveRecord::Base.sanitize @app_bin_id} AND
+        transaction_type = 'background_job' AND
+        start > (current_timestamp - interval '1 day')
+      GROUP BY job_name
+      ORDER BY hits DESC;
+    SQL
+
+    ActiveRecord::Base.connection.execute sql
+  end
+
+  def process_tuples(tuples)
+    tuples.to_a.map do |tuple|
+      [
+        tuple['job_name'],
+        tuple['avg_duration'].to_f.round(2),
+        tuple['hits'],
+        tuple['avg_time_in_sql'].to_f.round(2),
+        tuple['avg_time_in_view'].to_f.round(2),
+        tuple['avg_time_in_controller'].to_f.round(2),
+        tuple['avg_time_in_other'].to_f.round(2)
+      ]
+    end
+  end
+end
