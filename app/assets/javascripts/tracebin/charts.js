05487ed836d869fc66ca62db8010cda924943e27
Tyler Guillen <tyguillen@gmail.com>
2017-04-27 12:25:22 -0400

    add ui for cpu usage

--- app/assets/javascripts/tracebin/charts.js
+++ app/assets/javascripts/tracebin/charts.js
@@ -90,7 +90,7 @@ Tracebin.charts = {
       ],
 
       legend: {
-        position: 'top'
+        position: 'top',
       },
 
       chartArea: {
@@ -107,10 +107,62 @@ Tracebin.charts = {
       hAxis: {
         textStyle: Tracebin.chartStyles.hAxisText,
       },
-      isStacked: true
+      isStacked: true,
     }
 
     chart = new google.visualization.SteppedAreaChart(this);
+
+    chart.draw(data, options);
+  },
+
+  cpuMetricsShow: function(rawData) {
+    var formattedData = rawData.map(function(row) {
+      return [new Date(row[0]), row[1] * 100];
+    });
+
+    var data = new google.visualization.DataTable();
+    var options;
+    var chart;
+
+    data.addColumn('datetime', 'Interval');
+    data.addColumn('number', 'Usage');
+
+    data.addRows(formattedData);
+
+    options = {
+      title: 'CPU Usage',
+      titleTextStyle: Tracebin.chartStyles.titleText,
+
+      height: 300,
+      fontName: 'Abel',
+
+      colors: [
+        Tracebin.styles.colors.blue,
+      ],
+
+      legend: {
+        position: 'top',
+      },
+
+      chartArea: {
+        width: 700,
+      },
+
+      vAxis: {
+        title: 'Percent',
+        titleTextStyle: Tracebin.chartStyles.vAxisTitleText,
+
+        textStyle: Tracebin.chartStyles.vAxisText,
+
+        maxValue: 100,
+      },
+
+      hAxis: {
+        textStyle: Tracebin.chartStyles.hAxisText,
+      },
+    };
+
+    chart = new google.visualization.LineChart(this);
 
     chart.draw(data, options);
   },
--- app/services/cpu_metrics_show_data.rb
+++ app/services/cpu_metrics_show_data.rb
@@ -13,13 +13,13 @@ class CpuMetricsShowData
   def fetch_tuples
     sql = <<~SQL
       SELECT
-        date_trunc('hour', ct.start) AS interval,
-        round(avg((metrics->'cpu'->>'usage')::NUMERIC))
-          AS usage,
+        date_trunc('minute', sampled_at) AS interval,
+        avg((metrics->'cpu'->>'usage')::NUMERIC)
+          AS usage
       FROM system_health_samples
       WHERE
         app_bin_id = #{ActiveRecord::Base.sanitize @app_bin_id} AND
-        date_trunc('hour', ct.start) > (current_timestamp - interval '1 day')
+        date_trunc('minute', sampled_at) > (current_timestamp - interval '1 day')
       GROUP BY interval
       ORDER BY interval ASC;
     SQL
@@ -31,7 +31,7 @@ class CpuMetricsShowData
     tuples.map do |tuple|
       [
         tuple['interval'],
-        tuple['usage'].to_f.round(1)
+        tuple['usage'].to_f.round(3)
       ]
     end
   end
